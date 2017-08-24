4d9e6998fa51bc2aefd771e366ff039c0e795313
yang <yang@yangdeMacBook-Pro.local>
2017-08-24 23:35:17 +0800

    first

---
+++ OCTutorial/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OCTutorial
+//
+//  Created by yang on 2017/8/24.
+//  Copyright © 2017年 yang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
