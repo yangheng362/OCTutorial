69ea46b20ce1b2c9f85a4f520fd139430d47e25c
yang <yang@yangdeMacBook-Pro.local>
2017-08-24 23:55:12 +0800

    string

--- OCTutorial/main.m
+++ OCTutorial/main.m
@@ -11,6 +11,13 @@
 int main(int argc,char *args[]) {
     @autoreleasepool {
         NSLog(@"hello oc");
+        NSString *myString = @"hello,formate";
+        NSString *anotherString =[NSString stringWithFormat:@"%d,%@",1,myString];
+        NSLog(anotherString);
+        
+        NSString *createFromC = [NSString stringWithCString:"hello,c string" encoding:NSUTF8StringEncoding];
+        NSLog(createFromC);
+        
     }
     return 0;
 }
